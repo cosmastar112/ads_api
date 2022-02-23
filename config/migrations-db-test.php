28da182dd28383fa3a6f5cb02851024b96f443f4
cosmastar112 <cosmastar112@gmail.com>
2022-02-23 19:57:07 +0400

    unit-тесты models/rep/AdRep
    TODO: возможно, стоит переделать в функциональный тест
    
    конфиг миграций тестовой БД

---
+++ config/migrations-db-test.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'dbname' => 'ads-test',
+    'user' => 'ad-api',
+    'password' => 'password',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+];
---
+++ tests/unit/models/rep/AdRepTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace tests\unit\models\rep;
+
+use models\rep\AdRep;
+use models\Ad;
+
+class AdRepTest extends \Codeception\Test\Unit
+{
+    protected function setUp(): void
+    {
+        $this->getDb()->query('DELETE FROM ad; ALTER TABLE ad AUTO_INCREMENT = 1;');
+    }
+
+    protected function tearDown(): void
+    {
+       $this->getDb()->query('DELETE FROM ad; ALTER TABLE ad AUTO_INCREMENT = 1;');
+    }
+
+    public function testSave()
+    {
+        $text = 'text value';
+        $price = 100;
+        $limit = 1000;
+        $banner = 'banner value';
+        $model = new Ad(null, $text, $price, $limit, $banner);
+
+        $db = $this->getDb();
+        $rep = new AdRep($db);
+        $result = $rep->save($model);
+
+        $this->assertEquals(true, $result);
+        $this->getModule('Db')->seeInDatabase('ad', [
+            'text' => $text,
+            'price' => $price,
+            'limit' => $limit,
+            'banner' => $banner,
+        ]);
+    }
+
+    public function testGet()
+    {
+        //в БД нет записи
+        $id = 1;
+        $db = $this->getDb();
+        $rep = new AdRep($db);
+        $result = $rep->get($id);
+        $this->assertEquals(false, $result);
+
+        //в БД есть запись
+        $this->_setUpBeforeGet();
+        $id = 1;
+        $db = $this->getDb();
+        $rep = new AdRep($db);
+        $result = $rep->get($id);
+        $this->assertIsArray($result);
+    }
+
+    public function testGetRelevant()
+    {
+        //в БД нет записей
+        $db = $this->getDb();
+        $rep = new AdRep($db);
+        $result = $rep->getRelevant();
+        $this->assertEquals(false, $result);
+
+        //в БД есть записи
+        $this->_setUpBeforeGetRelevant(100, 5);
+        $this->_setUpBeforeGetRelevant(350, 5); //id=2
+        $this->_setUpBeforeGetRelevant(200, 5);
+        $db = $this->getDb();
+        $rep = new AdRep($db);
+        $result = $rep->getRelevant();
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals($result['id'], 2); //запись с максимальной ценой
+        $this->assertArrayHasKey('limit', $result);
+        $this->assertEquals($result['limit'], 4); //после показа счетчик уменьшится на 1
+    }
+
+    public function testGetRelevantIfZeroLimit()
+    {
+        //в БД есть запись, но лимит показов израсходован
+        $this->_setUpBeforeGetRelevant(100, 0);
+        $db = $this->getDb();
+        $rep = new AdRep($db);
+        $result = $rep->getRelevant();
+        $this->assertEquals(false, $result);
+    }
+
+    private function _setUpBeforeGet()
+    {
+        $text = 'text value';
+        $price = 100;
+        $limit = 1000;
+        $banner = 'banner value';
+        //создать запись
+        $st = $this->getDb()->prepare('INSERT INTO ad(`text`, price, `limit`, banner) VALUES(:text, :price, :limit, :banner)');
+        $st->execute([':text' => $text, ':price' => $price, ':limit' => $limit, ':banner' => $banner]);
+    }
+
+    private function _setUpBeforeGetRelevant($price, $limit)
+    {
+        $text = 'text value';
+        $price = $price;
+        $limit = $limit;
+        $banner = 'banner value';
+        //создать запись
+        $st = $this->getDb()->prepare('INSERT INTO ad(`text`, price, `limit`, banner) VALUES(:text, :price, :limit, :banner)');
+        $st->execute([':text' => $text, ':price' => $price, ':limit' => $limit, ':banner' => $banner]);
+    }
+
+    private function getDb()
+    {
+        $dbModule = $this->getModule('Db');
+
+        return $dbModule->driver->getDbh();
+    }
+}
