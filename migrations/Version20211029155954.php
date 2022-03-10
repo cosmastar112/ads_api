e7b14ca847fc56cbf610ab6275da9eeec5df2ae1
cosmastar112 <cosmastar112@gmail.com>
2022-03-10 23:14:01 +0400

    использовать в миграциях ORM вместо SQL

--- migrations/Version20211029155954.php
+++ migrations/Version20211029155954.php
@@ -19,22 +19,30 @@ final class Version20211029155954 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('
-
-CREATE TABLE `ad` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `text` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
-  `limit` int(11) DEFAULT NULL,
-  `banner` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-        ');
+        $table = $schema->createTable('ad');
+        $table->addColumn('id', 'integer', [
+            'autoincrement' => true,
+            'notnull' => true,
+        ]);
+        $table->addColumn('text', 'string', [
+            'default' => null,
+            'length' => 100,
+        ]);
+        $table->addColumn('price', 'integer', [
+            'default' => null,
+        ]);
+        $table->addColumn('limit', 'integer', [
+            'default' => null,
+        ]);
+        $table->addColumn('banner', 'string', [
+            'default' => null,
+            'length' => 100,
+        ]);
+        $table->setPrimaryKey(['id']);
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE `ad`');
+        $schema->dropTable('ad');
     }
 }
