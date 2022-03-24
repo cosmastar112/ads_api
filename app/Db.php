57bc6655cd8aa1ad3e568ee747da2a5e30b3dafb
cosmastar112 <cosmastar112@gmail.com>
2022-03-24 23:44:01 +0400

    документирование Db

--- app/Db.php
+++ app/Db.php
@@ -4,13 +4,26 @@ namespace app;
 
 use PDO;
 
+/**
+ * Обёртка для работы с БД (PDO).
+ */
 class Db
 {
+    /**
+     * Соединение с БД.
+     * @internal
+     * @var \PDO
+     */
     private $_instance;
 
+    /**
+     * @param array $config Конфиг БД.
+     * @throws \Exception Выбрасывается если файл БД sqlite не был найден.
+     */
     public function __construct($config)
     {
         if ($config['driver'] === 'mysql') {
+            /** @var string $dsn Имя источника данных или DSN, содержащее информацию, необходимую для подключения к базе данных. */
             $dsn = implode([
                 $config['driver'], ':',
                 'host=', $config['host'], ';',
@@ -18,35 +31,54 @@ class Db
             ]);
             $this->_instance = new PDO($dsn, $config['username'], $config['password']);
         } elseif ($config['driver'] === 'sqlite') {
-            //относительный путь к БД
+            /** @var string Относительный путь к БД sqlite. */
             $pathToDb = $config['path'];
             if (file_exists($pathToDb)) {
+                /** @var string $dsn Имя источника данных или DSN, содержащее информацию, необходимую для подключения к базе данных. */
                 $dsn = implode([ $config['driver'], ':', $pathToDb ]);
                 $this->_instance = new PDO($dsn);
             } else {
+                /** @var string $absPath Абсолютный путь (полученный из указанного относительного пути) до файла БД, который не существует. */
                 $absPath = $this->getRealPath($pathToDb);
                 throw new \Exception("Не удалось подключиться к БД: файл не найден ($absPath)");
             }
         }
     }
 
+    /**
+     * Соединение с БД.
+     * @return \PDO
+     */
     public function getInstance()
     {
        return $this->_instance;
     }
 
+    /**
+     * Закрыть соединение с БД.
+     * @return void
+     */
     public function closeInstance()
     {
         $this->_instance = null;
     }
 
-    //абсолютный путь до несуществующего файла
-    //сделано, т.к. realpath не работает с несуществующими файлами
+    /**
+     * Абсолютный путь до файла (работает и для несуществующего файла).
+     * @internal
+     * @param string $pathToDb Относительный путь.
+     * Добавлено в связи с тем, что {@link https://www.php.net/manual/ru/function.realpath.php realpath()} не работает с несуществующими файлами.
+     * @return string
+     */
     private function getRealPath($pathToDb)
     {
+        /** @var array $pathinfo Информация о path в виде ассоциативного массива. */
         $pathinfo = pathinfo($pathToDb);
+        /** @var string $dirname Наименование директории. */
         $dirname = $pathinfo['dirname'];
+        /** @var string $file Наименование файла (с расширением). */
         $file = $pathinfo['basename'];
+        /** @var string $absPath Абсолютный путь до файла. */
         $absPath = realpath($dirname) . '\\' . $file;
 
         return $absPath;
